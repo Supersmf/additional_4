b17e354dd529322f96b43b7f6c733232abcaa0ee
Supersmf <Supersmf@mail.ru>
2017-09-25 21:00:38 +0300

    additional_4/feature: done 100% of tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let one = first.split('');
+	let two = second.split('');
+	let sum = Array(one.length + two.length).fill(0);
+
+	for(let i = one.length-1; i > -1; i--){
+		for(let j = two.length-1; j > -1; j--){
+			
+			let temp = (one[i] * two[j]) + sum[i+j+1];
+			if(temp >= 10){
+				sum[i+j+1] = temp%10;
+				sum[i+j] += Math.floor(temp/10);
+			}else sum[i+j+1] = temp;
+		}
+	}
+	while(sum[0] === 0)sum.shift();
+	return sum.join('');
 }
